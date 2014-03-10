183b33314e0b6b24d4c155ab4497b66680a2acfc
Matt Calthrop <mcalthrop@gmail.com>
2014-03-10 17:03:19 +0000

    Added tests for different fonts

--- test-spec.js
+++ test-spec.js
@@ -6,19 +6,87 @@ describe('simple protractor test', function () {
     beforeEach(function () {
         locations = {
             angularjs: 'http://www.angularjs.org',
-            mcalthrop: 'http://mcalthrop.github.io/protractor-phantomjs-testing.html'
+            mcalthropWithEotFont: 'http://mcalthrop.github.io/protractor-phantomjs-testing-with-eot-font.html',
+            mcalthropWithSvgFont: 'http://mcalthrop.github.io/protractor-phantomjs-testing-with-svg-font.html',
+            mcalthropWithTtfFont: 'http://mcalthrop.github.io/protractor-phantomjs-testing-with-ttf-font.html',
+            mcalthropWithWoffFont: 'http://mcalthrop.github.io/protractor-phantomjs-testing-with-woff-font.html',
+            mcalthropWithAllFonts: 'http://mcalthrop.github.io/protractor-phantomjs-testing-with-fonts.html'
         };
     });
 
-    it('mcalthrop: should get text from named id', function () {
-        var location = locations.mcalthrop;
+    it('mcalthrop (with all EOT font): should get text from named id', function () {
+        var location = locations.mcalthropWithEotFont;
 
         console.log('Browsing', location);
         browser.get(location);
         expect(element(by.id('test-id')).getText()).toEqual('test text');
     });
-    it('mcalthrop: should pass basic test of data binding', function () {
-        var location = locations.mcalthrop,
+    it('mcalthrop (with all EOT font): should pass basic test of data binding', function () {
+        var location = locations.mcalthropWithEotFont,
+            name = 'Bruce';
+
+        console.log('Browsing', location);
+        browser.get(location);
+        element(by.model('yourName')).sendKeys(name);
+        expect(element(by.binding('yourName')).getText()).toEqual('Hello ' + name);
+    });
+    it('mcalthrop (with SVG font): should get text from named id', function () {
+        var location = locations.mcalthropWithSvgFont;
+
+        console.log('Browsing', location);
+        browser.get(location);
+        expect(element(by.id('test-id')).getText()).toEqual('test text');
+    });
+    it('mcalthrop (with SVG font): should pass basic test of data binding', function () {
+        var location = locations.mcalthropWithSvgFont,
+            name = 'Bruce';
+
+        console.log('Browsing', location);
+        browser.get(location);
+        element(by.model('yourName')).sendKeys(name);
+        expect(element(by.binding('yourName')).getText()).toEqual('Hello ' + name);
+    });
+    it('mcalthrop (with TTF font): should get text from named id', function () {
+        var location = locations.mcalthropWithTtfFont;
+
+        console.log('Browsing', location);
+        browser.get(location);
+        expect(element(by.id('test-id')).getText()).toEqual('test text');
+    });
+    it('mcalthrop (with TTF font): should pass basic test of data binding', function () {
+        var location = locations.mcalthropWithTtfFont,
+            name = 'Bruce';
+
+        console.log('Browsing', location);
+        browser.get(location);
+        element(by.model('yourName')).sendKeys(name);
+        expect(element(by.binding('yourName')).getText()).toEqual('Hello ' + name);
+    });
+    it('mcalthrop (with WOFF font): should get text from named id', function () {
+        var location = locations.mcalthropWithWoffFont;
+
+        console.log('Browsing', location);
+        browser.get(location);
+        expect(element(by.id('test-id')).getText()).toEqual('test text');
+    });
+    it('mcalthrop (with WOFF font): should pass basic test of data binding', function () {
+        var location = locations.mcalthropWithWoffFont,
+            name = 'Bruce';
+
+        console.log('Browsing', location);
+        browser.get(location);
+        element(by.model('yourName')).sendKeys(name);
+        expect(element(by.binding('yourName')).getText()).toEqual('Hello ' + name);
+    });
+    it('mcalthrop (with all fonts): should get text from named id', function () {
+        var location = locations.mcalthropWithAllFonts;
+
+        console.log('Browsing', location);
+        browser.get(location);
+        expect(element(by.id('test-id')).getText()).toEqual('test text');
+    });
+    it('mcalthrop (with all fonts): should pass basic test of data binding', function () {
+        var location = locations.mcalthropWithAllFonts,
             name = 'Bruce';
 
         console.log('Browsing', location);
