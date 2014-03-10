549a3c13022f15282524ce2b9a249334dbb7768c
Matt Calthrop <mcalthrop@gmail.com>
2014-03-10 15:50:16 +0000

    v0.0.3 Updated tests to use mcalthrop.github.io

--- test-spec.js
+++ test-spec.js
@@ -1,11 +1,38 @@
-/*global describe, element, expect, browser, it, by */
-describe('angularjs homepage', function() {
-    var name = 'Bruce';
+/*global describe, beforeEach, element, expect, browser, it, by */
 
-    it('should greet the named user', function() {
-        browser.get('http://www.angularjs.org');
+describe('simple protractor test', function () {
+    var locations;
+
+    beforeEach(function () {
+        locations = {
+            angularjs: 'http://www.angularjs.org',
+            mcalthrop: 'http://mcalthrop.github.io/protractor-phantomjs-testing.html'
+        };
+    });
+
+    xit('angularjs: should pass basic test of data binding on AngularJS page', function () {
+        var location = locations.angularjs,
+            name = 'Bruce';
+
+        console.log('Browsing', location);
+        browser.get(location);
         element(by.model('yourName')).sendKeys(name);
-        var greeting = element(by.binding('yourName'));
-        expect(greeting.getText()).toEqual('Hello ' + name + '!');
+        expect(element(by.binding('yourName')).getText()).toEqual('Hello ' + name + '!');
+    });
+    it('mcalthrop: should get text from named id', function () {
+        var location = locations.mcalthrop;
+
+        console.log('Browsing', location);
+        browser.get(location);
+        expect(element(by.id('test-id')).getText()).toEqual('test text');
+    });
+    it('mcalthrop: should pass basic test of data binding on AngularJS page', function () {
+        var location = locations.mcalthrop,
+            name = 'Bruce';
+
+        console.log('Browsing', location);
+        browser.get(location);
+        element(by.model('yourName')).sendKeys(name);
+        expect(element(by.binding('yourName')).getText()).toEqual('Hello ' + name);
     });
 });
