f5a023133d53644b509dc8719ec9599ad48acf0e
adi <adinaths@gmail.com>
2015-05-07 22:12:19 +0530

    start standalone selenium server instead of separately

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,7 +1,9 @@
 /*global exports */
 
 exports.config = {
-    seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
+    seleniumServerJar: require('selenium-server-standalone-jar').path,
+    //'node_modules/selenium-server-standalone-jar/jar/selenium-server-standalone-2.45.0.jar',
+    //seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
     seleniumArgs: ['-browserTimeout=60'],
     specs: [
         './test-spec.js'
@@ -14,7 +16,7 @@ exports.config = {
 */
         {
             'browserName': 'phantomjs',
-            'phantomjs.binary.path': './node_modules/phantomjs/bin/phantomjs',
+            'phantomjs.binary.path': require('phantomjs').path,
             'phantomjs.cli.args': ['--debug=false', '--webdriver-logfile=/tmp/webdriver.log', '--webdriver-loglevel=INFO']
 //            'phantomjs.cli.args': ['--debug=true', '--webdriver-logfile=webdriver.log', '--webdriver-loglevel=DEBUG']     // alternative, with full debugging on
             // (loglevels supported: 'ERROR', 'WARN', 'INFO', 'DEBUG') (default 'INFO')
